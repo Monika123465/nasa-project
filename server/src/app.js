96756be398567d1b5154cf0efee7ab76f21a6a47
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2022-12-23 19:41:46 +0530

    done project nasa

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,26 @@
+const path=require('path')
 const express=require("express")
 const cors=require("cors")
+const morgan=require('morgan')
 const planetRouter=require("./routes/planets/plantes.router")
+const launchRouter=require("./routes/launches/launches.router")
+
 
 const app=express()
 
 app.use(cors({
     origin:'http://localhost:3000'
 }))
+
+app.use(morgan('combined'))
 app.use(express.json())
 app.use(planetRouter)
+app.use(launchRouter)
+app.use(express.static(path.join(__dirname,"..",'public')))
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'..','public','index.html'))
+})
+
 
 module.exports=app
